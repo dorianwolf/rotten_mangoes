e9c99ee29ec21c5f7cb6c4156af4faab0f23f04e
dorianwolf <dorian.wolf@gmail.com>
2015-11-17 04:27:37 +0000

    finished

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -6,6 +6,10 @@ class Movie < ActiveRecord::Base
   validates :runtime_in_minutes, numericality: { only_integer: true }
   validate :release_date_is_in_the_past
 
+  def review_average
+    reviews.sum(:rating_out_of_ten)/reviews.size if reviews.size > 0
+  end
+
   protected
 
   def release_date_is_in_the_past
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ActiveRecord::Base
 
   validates :email, :firstname, :lastname, presence: true
   has_secure_password
+
+  def full_name
+    "#{firstname} #{lastname}"
+  end
+
 end
