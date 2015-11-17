3fe439d19810378eec3d349b8ca1bace8221abfc
dorianwolf <dorian.wolf@gmail.com>
2015-11-17 21:04:54 +0000

    admin user-list features

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
   before_action :check_admin
 
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(10)
   end
 
   def show
