bef4947d1419e113837413c5185aff8e8f15b3a7
dorianwolf <dorian.wolf@gmail.com>
2015-11-17 00:41:11 +0000

    up to exercise 6

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+
+  validates :title, :director, :description, :release_date, presence: true
+  validates :runtime_in_minutes, numericality: { only_integer: true }
+  validate :release_date_is_in_the_past
+
+  protected
+
+  def release_date_is_in_the_past
+    if release_date.present?
+      errors.add(:release_date, "should be in the past") if release_date > Date.today
+    end
+  end
+
 end
