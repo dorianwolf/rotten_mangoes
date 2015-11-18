5db426b2e3f222a7e3b443b090fba8fea572a424
dorianwolf <dorian.wolf@gmail.com>
2015-11-18 21:19:28 +0000

    send delete notification email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
+    UserMailer.delete_confirmation(@user).deliver
     @user.destroy
     session.clear if params[:id] == session[:id]
     redirect_to users_path
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "man@mangoteam.com"
+  layout 'mailer'
+end
